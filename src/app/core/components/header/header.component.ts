eb7456c781ddab23c626a7becceff41f32ce3a41
akash <akashdeep9226@gmail.com>
2024-03-17 00:28:12 +0530

    changes fix header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { TopBarComponent } from '../top-bar/top-bar.component';
 import { LogoComponent } from '../logo/logo.component';
 import { slideInAnimation } from '../../models';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TopBarComponent,LogoComponent],
+  imports: [TopBarComponent,NgbCollapseModule,LogoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   animations: [slideInAnimation]
 })
 export class HeaderComponent {
-
+  isCollapsed : boolean= false;
 }
